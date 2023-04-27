f61284ef29631982fe4300bd5553768b6dd95e57
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-04-27 15:16:22 +0200

    Changes by beccio

--- Code/source/src/data_access/ProfessorGateway.java
+++ Code/source/src/data_access/ProfessorGateway.java
@@ -45,18 +45,15 @@ public class ProfessorGateway implements Gateway{
         statement.close();
     }
 
-    //FIXME
     public int getGrade(Student student) throws SQLException {
 
         int grade;
 
         //SQL per ottenere il voto dello studente fornito
         String gradeSQL = """
-            SELECT Esame.Voto
-            FROM Esame
-            JOIN Libretto ON Esame.Codice = Libretto.Esame
-            JOIN Studente ON Libretto.Codice = Studente.Libretto
-            WHERE Esame.Corso = ? AND Studente.Matricola = ?""";
+                SELECT Voto
+                FROM Esame
+                WHERE Corso = ? AND Studente = ?""";
 
         PreparedStatement gradeStatement = connection.prepareStatement(gradeSQL);
         gradeStatement.setString(1, this.courseID);
@@ -76,7 +73,7 @@ public class ProfessorGateway implements Gateway{
     }
 
     public void setGrade(Student student, Exam exam, int grade) throws SQLException{
-        String sql = "INSERT OR IGNORE INTO Esame (Codice, Nome, Data, CFU, Voto, Corso, TipoEsame) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO Esame (Codice, Nome, Studente, Data, CFU, Voto, Corso, TipoEsame) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, exam.getId());
@@ -90,27 +87,14 @@ public class ProfessorGateway implements Gateway{
 
         statement.executeUpdate();
         statement.close();
-
-        //Aggiungo l'esame al libretto dello studente
-        String transcriptSql = """
-                UPDATE Libretto
-                SET Esame = ?
-                WHERE Codice = ?""";
-
-        PreparedStatement transcriptStatement = connection.prepareStatement(transcriptSql);
-        transcriptStatement.setString(1, exam.getId());
-        transcriptStatement.setString(2, student.getUniTranscript().getId());
-
-        transcriptStatement.executeUpdate();
-        transcriptStatement.close();
     }
 
     // Ritorna la media su tutti gli esami dati dallo studente
     public float getAverage(Student student) throws SQLException, Exception{
         String average = """
-                SELECT AVG(Esame.voto)
-                FROM (Studente JOIN Libretto ON Studente.Libretto = Libretto.codice) JOIN Esame ON Libretto.Esame = Esame.Codice
-                WHERE Studente.Matricola = ?""";
+                SELECT AVG(Voto)
+                FROM Esame
+                WHERE Esame.Studente = ?""";
 
         PreparedStatement averageStatement = connection.prepareStatement(average);
         averageStatement.setString(1, student.getId());
