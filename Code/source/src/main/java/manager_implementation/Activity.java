5d66671db620087ac643388f75474c3a7e80c759
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-05-05 14:30:55 +0200

    Creata la classe Activity

---
+++ Code/source/src/main/java/manager_implementation/Activity.java
@@ -0,0 +1,57 @@
+package manager_implementation;
+
+import domain_model.RandomStringGenerator;
+
+public class Activity {
+
+    public Activity() {
+        this.id = RandomStringGenerator.generateRandomString(8);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    private String id;
+    private String name;
+    private String date;
+    private int startTime;
+    private int endTime;
+
+}
