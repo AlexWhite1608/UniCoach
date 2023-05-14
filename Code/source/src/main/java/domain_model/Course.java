177d0c8567d1d2eceefebe7d44456fe43c14f381
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-05-14 11:50:14 +0200

    todo in course.addCourse()

--- Code/source/src/main/java/domain_model/Course.java
+++ Code/source/src/main/java/domain_model/Course.java
@@ -36,6 +36,7 @@ public class Course {
     }
 
     //Aggiunge il corso al db quando viene istanziato
+    //TODO: aggiungi anche l'esame nella tabella Esame con valori Data e Voto nulli!
     private void addCourse() throws SQLException {
         Connection connection = DBConnection.connect("../database/unicoachdb.db");
 
